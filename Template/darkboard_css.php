8fcee02675b81a273b1c379c8badc7535a0f287b
Manuel <info@tagirijus.de>
2023-06-06 11:16:13 +0200

    Fixed wrong redirect after login

--- Template/darkboard_css.php
+++ Template/darkboard_css.php
@@ -1,1 +1,25 @@
-<link rel="stylesheet" href="/darkboard/css">
+<?php
+
+    // If I do not save the eventually saved variable,
+    // the access to the Darkboard generated CSS route
+    // will be count as "No access" and save this url
+    // into the "redirectAfterLogin" key of the SESSION
+    if (array_key_exists('redirectAfterLogin', $_SESSION)) {
+        $saveRedirectAfterLogin = $_SESSION['redirectAfterLogin'];
+        // if this variable is the darkboard css route, simply get rid of it
+        if ($saveRedirectAfterLogin == '/darkboard/css') {
+            $saveRedirectAfterLogin = '/';
+        }
+    }
+
+?>
+
+<link rel="stylesheet" href="/darkboard/css" media="screen">
+
+<?php
+
+    if (array_key_exists('redirectAfterLogin', $_SESSION)) {
+        $_SESSION['redirectAfterLogin'] = $saveRedirectAfterLogin;
+    }
+
+?>
